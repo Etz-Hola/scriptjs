c49cba513a4daa59c371d1a812c2c2d163c3f335
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-09-08 10:11:03 +0100

    up

---
+++ 12-lesson/script.js
@@ -0,0 +1,18 @@
+console.log("LESSON 12 IN JAVAS.");
+
+const view1 = document.getElementById('view1');
+console.log(view1);
+
+const view2 = document.querySelector('#view2');
+console.log(view2);
+
+const view = document.getElementsByClassName('view');
+console.log(view);
+
+const views = document.querySelectorAll('');
+
+
+
+
+view1.style.display = 'flex';
+view2.style.display = 'none';
